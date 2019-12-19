4d23828387ff87452e35a4a545324a4ccf572165
RKudakov <rkudakov@bit-erp.ru>
2019-12-19 17:04:46 +0300

    Переделать на static либу. Работает как приложение, не работает как so

--- PinkRabbitMQLinux/src/RMQConnection.cpp
+++ PinkRabbitMQLinux/src/RMQConnection.cpp
@@ -3,6 +3,7 @@
 #include "event2/event.h"
 #include <amqpcpp.h>
 #include <amqpcpp/libevent.h>
+#include "MyTCPHandler.cpp"
 
 bool RMQConnection::connect() {
 	
@@ -11,7 +12,7 @@ bool RMQConnection::connect() {
 
     // handler for libevent
     AMQP::LibEventHandler handler(evbase);
-
+    
     // make a connection
     AMQP::TcpConnection connection(&handler, AMQP::Address("amqp://rkudakov_devops:rkudakov_devops@devdevopsrmq.bit-erp.loc/rkudakov_devops"));
 
@@ -19,7 +20,7 @@ bool RMQConnection::connect() {
     AMQP::TcpChannel channel(&connection);
 
     // create a temporary queue
-    channel.declareQueue("my_Tests").onSuccess([&connection](const std::string& name, uint32_t messagecount, uint32_t consumercount) {
+    channel.declareQueue("my_Tests3").onSuccess([&connection](const std::string& name, uint32_t messagecount, uint32_t consumercount) {
 
         // report the name of the temporary queue
         std::cout << "declared queue " << name << std::endl;
@@ -34,5 +35,6 @@ bool RMQConnection::connect() {
     event_base_free(evbase);
 
     // done
+    
     return 0;
 }
