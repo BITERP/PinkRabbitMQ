32cf719922b7c431fdf43fafc03e0513ef08c123
vdykov <vdykov@bit-erp.ru>
2022-02-16 07:39:24 +0300

    DEVOPS-8497 Откатил имя компоненты

--- src/RabbitMQClientNative.cpp
+++ src/RabbitMQClientNative.cpp
@@ -87,7 +87,7 @@ static const char16_t* g_MethodNamesRu[] = {
 	u"SleepNative",
 };
 
-const char16_t* RabbitMQClientNative::componentName = u"PinkRabbitMQ3";
+const char16_t* RabbitMQClientNative::componentName = u"PinkRabbitMQ";
 
 // CAddInNative
 //---------------------------------------------------------------------------//
